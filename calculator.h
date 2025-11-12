7e93fead4852ed3fb448682a3c1ff4cbbfe49d17
WeiZhe <41343134@nfu.edu.tw>
2025-11-12 20:04:20 +0800

    calculator

---
+++ calculator.h
@@ -0,0 +1,23 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class calculator;
+}
+QT_END_NAMESPACE
+
+class calculator : public QWidget
+{
+    Q_OBJECT
+
+public:
+    calculator(QWidget *parent = nullptr);
+    ~calculator();
+
+private:
+    Ui::calculator *ui;
+};
+#endif // CALCULATOR_H
